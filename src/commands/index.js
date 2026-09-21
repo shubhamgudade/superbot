b564a3a4430339d895a6bb460180cfdf9767a010
shubhamgudade <71931048+shubhamgudade@users.noreply.github.com>
2026-09-21 23:24:44 +0530

    feat: register opno command

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,10 +1,11 @@
 import hi from "./hi.js";
 import setprefix from "./setprefix.js";
 import sticker from "./sticker.js";
+import opno from "./opno.js";
 
 const commands = new Map();
 
-for (const command of [hi, setprefix, sticker]) {
+for (const command of [hi, setprefix, sticker, opno]) {
   commands.set(command.name, command);
 
   for (const alias of command.aliases || []) {
