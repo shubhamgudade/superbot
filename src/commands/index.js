0537fb48c8a809003949d8f7fedafcde9984371e
shubhamgudade <71931048+shubhamgudade@users.noreply.github.com>
2026-09-21 23:47:03 +0530

    feat: register new image command

--- src/commands/index.js
+++ src/commands/index.js
@@ -2,10 +2,11 @@ import hi from "./hi.js";
 import setprefix from "./setprefix.js";
 import sticker from "./sticker.js";
 import opno from "./opno.js";
+import kirk from "./kirk.js";
 
 const commands = new Map();
 
-for (const command of [hi, setprefix, sticker, opno]) {
+for (const command of [hi, setprefix, sticker, opno, kirk]) {
   commands.set(command.name, command);
 
   for (const alias of command.aliases || []) {
