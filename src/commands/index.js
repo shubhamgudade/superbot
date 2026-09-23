6b483d87e4d66914d3f1f287132af3c5534fa6cf
shubhamgudade <71931048+shubhamgudade@users.noreply.github.com>
2026-09-23 16:00:26 +0530

    Register photo meme commands

--- src/commands/index.js
+++ src/commands/index.js
@@ -4,10 +4,11 @@ import sticker from "./sticker.js";
 import opno from "./opno.js";
 import kirk from "./kirk.js";
 import carbon from "./carbon.js";
+import { wanted, criminal, jail } from "./photoeffects.js";
 
 const commands = new Map();
 
-for (const command of [hi, setprefix, sticker, opno, kirk, carbon]) {
+for (const command of [hi, setprefix, sticker, opno, kirk, carbon, wanted, criminal, jail]) {
   commands.set(command.name, command);
 
   for (const alias of command.aliases || []) {
