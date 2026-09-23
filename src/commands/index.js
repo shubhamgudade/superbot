46a5fe46011d2817eb6f0e6874f9549da8a38b31
shubhamgudade <71931048+shubhamgudade@users.noreply.github.com>
2026-09-23 19:57:49 +0530

    Register JokeAPI, Digimon TCG, and Stardew commands

--- src/commands/index.js
+++ src/commands/index.js
@@ -7,13 +7,16 @@ import carbon from "./carbon.js";
 import pokemon from "./pokemon.js";
 import avatar from "./avatar.js";
 import chuck from "./chuck.js";
+import joke from "./joke.js";
 import fact from "./fact.js";
 import digimon from "./digimon.js";
+import tcg from "./tcg.js";
+import stardew from "./stardew.js";
 import { wanted, criminal, jail } from "./photoeffects.js";
 
 const commands = new Map();
 
-for (const command of [hi, setprefix, sticker, opno, kirk, carbon, pokemon, avatar, chuck, fact, digimon, wanted, criminal, jail]) {
+for (const command of [hi, setprefix, sticker, opno, kirk, carbon, pokemon, avatar, chuck, joke, fact, digimon, tcg, stardew, wanted, criminal, jail]) {
   commands.set(command.name, command);
 
   for (const alias of command.aliases || []) {
