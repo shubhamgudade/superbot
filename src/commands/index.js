dfdf831b8708341cefd34d873ad4f2549c71edb7
shubhamgudade <71931048+shubhamgudade@users.noreply.github.com>
2026-09-23 20:55:13 +0530

    Register Genderize command

--- src/commands/index.js
+++ src/commands/index.js
@@ -25,11 +25,12 @@ import musicapi from "./musicapi.js";
 import gaanaapi from "./gaanaapi.js";
 import movie from "./movie.js";
 import abhi from "./abhi.js";
+import gender from "./gender.js";
 import { wanted, criminal, jail } from "./photoeffects.js";
 
 const commands = new Map();
 
-for (const command of [hi, setprefix, sticker, opno, kirk, carbon, pokemon, avatar, chuck, joke, fact, digimon, tcg, stardew, mcu, lyrics, wiki, insult, indianquote, moviequote, covid, akshay, translate, musicapi, gaanaapi, movie, abhi, wanted, criminal, jail]) {
+for (const command of [hi, setprefix, sticker, opno, kirk, carbon, pokemon, avatar, chuck, joke, fact, digimon, tcg, stardew, mcu, lyrics, wiki, insult, indianquote, moviequote, covid, akshay, translate, musicapi, gaanaapi, movie, abhi, gender, wanted, criminal, jail]) {
   commands.set(command.name, command);
 
   for (const alias of command.aliases || []) {
