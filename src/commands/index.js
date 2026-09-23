5e4c9bf5712f93187df0e37037f7881076166ae7
shubhamgudade <71931048+shubhamgudade@users.noreply.github.com>
2026-09-23 19:45:50 +0530

    Register Pokemon command

--- src/commands/index.js
+++ src/commands/index.js
@@ -4,11 +4,12 @@ import sticker from "./sticker.js";
 import opno from "./opno.js";
 import kirk from "./kirk.js";
 import carbon from "./carbon.js";
+import pokemon from "./pokemon.js";
 import { wanted, criminal, jail } from "./photoeffects.js";
 
 const commands = new Map();
 
-for (const command of [hi, setprefix, sticker, opno, kirk, carbon, wanted, criminal, jail]) {
+for (const command of [hi, setprefix, sticker, opno, kirk, carbon, pokemon, wanted, criminal, jail]) {
   commands.set(command.name, command);
 
   for (const alias of command.aliases || []) {
