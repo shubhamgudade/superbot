5ae055925b69adee2c1fa819ee1176836191d3df
shubhamgudade <71931048+shubhamgudade@users.noreply.github.com>
2026-09-23 20:14:21 +0530

    Register API commands

--- src/commands/index.js
+++ src/commands/index.js
@@ -12,11 +12,22 @@ import fact from "./fact.js";
 import digimon from "./digimon.js";
 import tcg from "./tcg.js";
 import stardew from "./stardew.js";
+import mcu from "./mcu.js";
+import lyrics from "./lyrics.js";
+import wiki from "./wiki.js";
+import insult from "./insult.js";
+import indianquote from "./indianquote.js";
+import moviequote from "./moviequote.js";
+import covid from "./covid.js";
+import akshay from "./akshay.js";
+import translate from "./translate.js";
+import musicapi from "./musicapi.js";
+import gaanaapi from "./gaanaapi.js";
 import { wanted, criminal, jail } from "./photoeffects.js";
 
 const commands = new Map();
 
-for (const command of [hi, setprefix, sticker, opno, kirk, carbon, pokemon, avatar, chuck, joke, fact, digimon, tcg, stardew, wanted, criminal, jail]) {
+for (const command of [hi, setprefix, sticker, opno, kirk, carbon, pokemon, avatar, chuck, joke, fact, digimon, tcg, stardew, mcu, lyrics, wiki, insult, indianquote, moviequote, covid, akshay, translate, musicapi, gaanaapi, wanted, criminal, jail]) {
   commands.set(command.name, command);
 
   for (const alias of command.aliases || []) {
